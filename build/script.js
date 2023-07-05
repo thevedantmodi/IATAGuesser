dd6f1d6215163bf1736970046ae5e216272b77b2
Vedant Modi <vedantmodi@gmail.com>
2023-07-05 16:09:12 -0500

    Test that js integrated properly

--- build/script.js
+++ build/script.js
@@ -1,6 +1,15 @@
 import { CODES } from "./codes.js";
 
+
 function printCode () {
+
+    let board = document.getElementById("game-board");
+
+    let code = document.createElement("p");
+    code.innerHTML = 'Hi!';
+    
+    board.appendChild(code);
+
     console.log(CODES[Math.floor(Math.random() * CODES.length)])
 }
 
