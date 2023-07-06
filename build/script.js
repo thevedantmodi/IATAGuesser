611ea4cfa2e5ca440a257ddec0093df0da57ba22
Vedant Modi <vedantmodi@gmail.com>
2023-07-06 10:34:17 -0500

    Added basic guess checking functionality

--- build/script.js
+++ build/script.js
@@ -1,14 +1,21 @@
 import { EASY_CODES } from "./codes.js";
 
 let guess = ""
+let chosen = ""
 const IATA_code = document.getElementById("IATA-code");
 const input = document.getElementById("input-field");
 const enter = document.getElementById("enter-btn");
 const output = document.getElementById("output");
 
 function printCode () {
-    let chosen = EASY_CODES[Math.floor(Math.random() * EASY_CODES.length)]
+    chosen = EASY_CODES[Math.floor(Math.random() * EASY_CODES.length)]
     IATA_code.innerHTML = chosen
+}
+
+function checkGuess (guess) {
+    if (guess == chosen) {
+        output.innerHTML = "Correct"
+    } 
 }
 
 printCode()
@@ -23,5 +30,6 @@ input.addEventListener("keypress", () => {
 enter.addEventListener("click", () => {
     guess = input.value
     input.value = ""
-    output.innerHTML = guess
 })
+
+checkGuess()
